25a6d35484b98008648af66848ddeba9716607fb
Alex Hemard <alexhemard@gmail.com>
2015-09-10 14:07:47 -0500

    update project.clj

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-http "2.0.0"]
+                 [com.amazonaws/aws-lambda-java-core "1.0.0"]
+                 [com.amazonaws/aws-lambda-java-events "1.0.0"]]
+  :aot :all)
